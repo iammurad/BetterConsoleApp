42ab9a96f7213b4454802405dbebb5ac6b7e3759
LinuxAlien <murik8900@gmail.com>
2022-04-24 18:16:29 +0400

    Calling entry point in Main()

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
 using System;
 using System.ComponentModel;
@@ -27,10 +26,13 @@ namespace ConsoleUI
             var host = Host.CreateDefaultBuilder()
                 .ConfigureServices((context, services) =>
                 {
-
+                    services.AddTransient<IGreetingService,GreetingService>();
                 })
                 .UseSerilog()
                 .Build();
+
+            var Myservice = ActivatorUtilities.CreateInstance<IGreetingService>(host.Services);
+            Myservice.Run();
         }
 
         static void BuildConfig(IConfigurationBuilder builder)
@@ -39,24 +41,6 @@ namespace ConsoleUI
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
                 .AddEnvironmentVariables();
-        }
-    }
-    public class GreetingService
-    {
-        private readonly ILogger<GreetingService>_log;
-        private readonly IConfiguration _config;
-
-        public GreetingService(ILogger<GreetingService> log,IConfiguration config)
-        {
-            _log = log;
-            _config = config;
-        }
-        public void Run()
-        {
-            for (int i = 0; i < 10; i++)
-            {
-
-            }
         }
     }
 }
